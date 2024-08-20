d737a363438bcc7963f32ff58f2a0e854b0a8b37
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-08-20 18:48:50 +0530

    tailwand setup done

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react' ;
+
+import HiredLogo from "../../../public/logo.png" ;
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar ;
+
+
